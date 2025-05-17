4cabb84e14d8bf1a831db248dd91b0e7071e0632
Jhoel Santivañez <143150471+jhoel2002@users.noreply.github.com>
2025-05-17 00:25:35 -0500

    Update AwsConfig.java

--- src/main/java/com/application/claimhereweb/AWS/AwsConfig.java
+++ src/main/java/com/application/claimhereweb/AWS/AwsConfig.java
@@ -9,6 +9,7 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Profile("!test")
